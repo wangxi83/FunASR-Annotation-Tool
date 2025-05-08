c431321749c13e64feea11389f581921fc6a77c3
wangxi <wangxi@sobey.com>
2025-05-08 14:15:40 +0800

    重构了一下界面元素，简化一下，没必要那么复杂

--- src_view/App.vue
+++ src_view/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import { ref } from 'vue';
+import {ref, useTemplateRef} from 'vue';
 import { onMounted } from 'vue';
 import { Document, ArrowLeft, ArrowRight, Headset,Check} from '@element-plus/icons-vue';
 import mic_red from '@/assets/mic.png';
 import mic_green from '@/assets/mic_green.png';
-import SentenceList from './views/SentenceList.vue';
 
 
 let filePath = ref(null);
-let fileContents = ref(null); // for debug
+let sentences = ref(null);
 let mic_status_pic = ref(mic_red);
 let start_record = ref(false);
 let end_record = ref(false);
@@ -18,11 +17,16 @@ let cur_sentence = ref(null);
 // onMounted(async ()=>{
 // });
 
-
 async function selectFile() {
   filePath.value = await window.eAPI.openFile();
   if(filePath.value){
-    fileContents.value = await window.eAPI.readTrainText(filePath.value);
+    let fileContents = await window.eAPI.readTrainText(filePath.value);
+    if(fileContents&&fileContents.length>0){
+      sentences.value = fileContents.map((s, index)=>{
+        return {i: s.i, c: s. c, index} //主要是增加一个index
+      })
+      onSentenceClick(sentences.value[0], 0);
+    }
   }
 }
 
@@ -38,15 +42,24 @@ async function endRecord(){
   end_record.value = true;
 }
 
-function onSentenceClick(sentence, i){
+
+function onSentenceClick(sentence){
   cur_sentence.value = sentence;
+}
+
+function seekTo(where){
+  if(where==='prev'){
+    onSentenceClick(sentences.value[cur_sentence.value.index-1]);
+  }else{
+    onSentenceClick(sentences.value[cur_sentence.value.index+1]);
+  }
 }
 </script>
 
 <template>
   <div class="main">
     <div class="recorder">
-      <div class="default" v-if="!Array.isArray(fileContents)||!fileContents.length">
+      <div class="default" v-if="!Array.isArray(sentences)||!sentences.length">
         <el-text class="mx-1 tips" type="info">点击下方按钮，选择语音材料以开始</el-text>
         <el-button type="primary" :icon="Document" @click="selectFile">选择材料文件</el-button>
       </div>
@@ -55,7 +68,7 @@ function onSentenceClick(sentence, i){
           <el-text class="mx-1 tips" type="info">按住语音按钮，以正常语速念出下述内容</el-text>
         </div>
         <div class="sentence">
-          <el-text class="mx-1 c-1">{{cur_sentence}}</el-text>
+          <el-text class="mx-1 c-1">{{cur_sentence.c}}</el-text>
         </div>
         <div class="confirm">
           <el-button :icon="Headset" circle />
@@ -66,15 +79,31 @@ function onSentenceClick(sentence, i){
           <span v-if="end_record">15″</span>
         </el-divider>
         <div class="controller">
-          <el-icon :size="20"><ArrowLeft /></el-icon>
+          <a @click="cur_sentence.index===0?null:seekTo('prev')">
+            <el-icon :size="20" class="pagectl prev" :class="cur_sentence.index===0?'disabled':null"><ArrowLeft /></el-icon>
+          </a>
           <img @touchstart.prevent="startRecord" @touchend.prevent="endRecord" :src="mic_status_pic" style="height: 48px; width: 48px"/>
-          <el-icon :size="20"><ArrowRight /></el-icon>
+          <a @click="cur_sentence.index===sentences.length-1?null:seekTo('next')">
+            <el-icon :size="20" class="pagectl next" :class="cur_sentence.index===sentences.length-1?'disabled':null"><ArrowRight /></el-icon>
+          </a>
         </div>
       </div>
     </div>
     <el-divider direction="vertical" style="height: 100%; border-color: #f3f3f3;"/>
     <div class="sentence-list">
-      <SentenceList :sentences="fileContents" @sentence-click="onSentenceClick"/>
+<!--      <SentenceList :sentences="sentences" @sentence-click="onSentenceClick"-->
+<!--                    ref="slist" :seek-to-idx="seek_to_idx"/>-->
+      <el-card v-for="(sentence, index) in sentences" shadow="never" @click="onSentenceClick(sentence, index)"
+               :id="sentence.i" :key="sentence.i" :class="cur_sentence.i===sentence.i?'selected':null">
+        <div class="sentence-card">
+          <div class="s-content" :class="''">
+            <el-text class="mx-1 c-1">{{ sentence.c }}</el-text>
+          </div>
+          <div class="s-status">
+            <el-icon :size="16"><SuccessFilled /></el-icon>
+          </div>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -110,12 +139,45 @@ function onSentenceClick(sentence, i){
       .controller {
         flex-grow: 0; height: 100px; display: flex; justify-content: space-between; align-items: center;
         width: 200px;
+
+        .pagectl {
+          color: var(--color-text);
+          &.disabled {
+            color: var(--el-border-color-light);
+          }
+        }
       }
     }
   }
 
   .sentence-list {
-    width: 30%;  overflow-y: auto;
+    width: 30%;  overflow-y: auto; scroll-behavior: smooth;
+    :deep(.el-card) {
+      margin: 4px 0 4px 0; cursor: pointer;
+      &:hover, &.selected {
+        background-color: var(--el-color-primary-light-9);
+      }
+
+      .el-card__body {
+        padding: 5px 10px;
+
+        .sentence-card {
+          display: flex; flex-direction: column;
+
+          .s-content {
+            flex-grow: 1;
+            .c-1 {
+              display: inline-block; text-align: left;
+            }
+          }
+
+          .s-status {
+            height: 20px; width: 100%; display: flex; justify-content: end; align-items: center;
+            color: var(--el-border-color-light);
+          }
+        }
+      }
+    }
   }
 }
 </style>
